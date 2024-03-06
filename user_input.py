2e948461cc0e705737f174803f9b8c71f6d51d89
immaculate <muliimaculate@gmail.com>
2024-03-06 21:06:43 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Ask the user for their name
+name = input("Enter your name: ")
+
+# Ask the user for their age
+age = input("Enter your age: ")
+
+# Ask the user for their location
+location = input("Enter your location: ")
+
+# Print out a personalized message
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
